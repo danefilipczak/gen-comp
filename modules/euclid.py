f3f5dec8198fcb2be1e67418ce0ed90d30b3bda6
Dane Filipczak <dane@imaginationrock.com>
2018-01-08 11:27:24 -0500

    updated euclid module

--- modules/euclid.py
+++ modules/euclid.py
@@ -1,4 +1,18 @@
+import music21 as m21
+import random
+from copy import deepcopy
+
+
 def bjorklund(pulses, steps, rotation=0):
+    '''
+            returns a list of binary values representing the attacks in a euclidean sequence, e.g. [1, 0, 1, 0, 1, 0, 0]
+            arguments:
+                pulses - the number of attacks to be spaced evenly 
+                steps - the number of units over which the pulses will be evenly spread. has to be greater than pulses
+                rotation - optionally rotate the rhythmic necklace
+
+            note that these arguments mirror and were inspired by the notation for generating euclidean sequences in tidalcyles
+    '''
     steps = int(steps)
     pulses = int(pulses)
     if pulses > steps:
@@ -39,3 +53,48 @@ def bjorklund(pulses, steps, rotation=0):
         return l[n:] + l[:n]
 
     return rotate(pattern[::-1], rotation)
+
+def euclidize(stream, length, pulses, steps, rotation=0):
+    '''
+        Given an input stream, slice it along a specified bjorklund sequence,
+        args:
+        length - the total length of the bjorklund sequence in quarterlengths, useful for incomplete measures
+        pulses, steps, rotation - bjorklund parameters (see above)
+    '''
+    euclidBinary = bjorklund(pulses, steps, rotation)
+    #convert the binary into a list of quarter lengths
+    euclidQuarterLengths = []
+    for i in range(0, len(euclidBinary)):
+        if euclidBinary[i] == 1:
+            euclidQuarterLengths.append((length/steps)*i)
+
+    return stream.sliceAtOffsets(euclidQuarterLengths, addTies=False, inPlace=True)
+
+def makeRandomEuclidean(score_, measureLength):
+    '''
+        Given a score with parts, randomly chop it up into euclidean sequences based on either sixteenth notes or eighth note triplets
+    '''
+    score = deepcopy(score_)
+    for p in score.parts:
+        for m in p.recurse(streamsOnly=True):
+            euclidize(m, measureLength, random.randint(5, 10), random.choice([16, 12]))
+
+    return score
+
+
+if __name__ == '__main__':
+    ''' some test functions '''
+    # print a bjorklund sequence
+    print(bjorklund(5, 9, 1))
+
+    s = m21.stream.Stream()
+    n = m21.note.Note()
+    n.duration.type = 'whole'
+    s.append(n)
+    #euclidize(s, 8, 12, 1).show()
+
+    b = m21.corpus.parse('palestrina/Agnus_01.krn')
+    euclid = makeRandomEuclidean(b, 8)
+    b.show()
+    euclid.show()
+
